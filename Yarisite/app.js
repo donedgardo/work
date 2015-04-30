16174aa7e11cbed662820ed6c14f10def6c16311
donedgardo <edgardo.g.carreras@gmail.com>
2015-04-29 23:10:40 -0400

    Make angular script a new file app.js

---
+++ Yarisite/app.js
@@ -0,0 +1,26 @@
+angular.module("yariWeb", [])
+.controller('MainCtrl', [function () {
+	self = this;
+
+	self.user = { admin: true};
+
+	//This variable will trigger ng-show to display the main container with the right content.
+	self.mainContent="home";
+
+	self.open = function(tab){
+		self.mainContent=tab;
+
+		//Test for debug.
+		console.log(self.mainContent + " is the selected tab.\n");
+	};
+
+}])
+.controller('ClientCtrl', [function() {
+	self = this;
+	self.user;
+	self.showClientForm=false;		
+	self.submit = function (){
+		console.log(self.user);
+	};
+}]);
+
